8d4ddfb707754d9813d5587b44da0233e4358bf2
Dan Valinotti <dvalinotti@icloud.com>
2019-02-24 18:01:19 -0500

    first milestone - header styles, login modal, button scrolling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+
 $(document).foundation();
+//smooth scrolling added to the "Get Started" button on the stop
+//goes to the id called "signup" at the bottom
+function startup() {
+			document.getElementById("signup").scrollIntoView({behavior:'smooth'});
+}
+function control_phone(){
+$('#phone-number-field').keyup(function(){
+		$(this).val($(this).val().replace(/(\d{3})\-?(\d{3})\-?(\d{4})/,'$1-$2-$3'))
+});
+}
